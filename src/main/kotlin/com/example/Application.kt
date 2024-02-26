6272dcecef103f4474e1d70e9123f40c86e4c483
A_V <82163471+Anton-vok@users.noreply.github.com>
2024-02-26 18:22:07 +0400

    a

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,10 +1,13 @@
 package com.example
 
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
+import io.ktor.server.html.*
 import io.ktor.server.http.content.*
 import io.ktor.server.netty.*
 import io.ktor.server.routing.*
+import kotlinx.html.*
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
@@ -13,9 +16,10 @@ fun main() {
 
 fun Application.module() {
     routing {
-        singlePageApplication {
-            react("static")
-        }
+//        static("/qr"){
+//            singlePageApplication {
+//                react("files")
+//            }
+        staticResources("/qr", "files")
     }
 }
-//a
